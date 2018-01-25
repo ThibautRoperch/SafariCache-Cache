fd9c495b4669e20b82b9707423f8aea70dba02ea
PierreGranier <piergranier@laposte.net>
2018-01-25 17:04:11 +0100

    Ajout de la musique de fond et du bouton mute

---
+++ js/musics.js
@@ -0,0 +1,14 @@
+function sound() {
+    var player = document.querySelector('#audioPlayer');
+
+    var note = document.querySelector(".note");
+
+    if (player.paused) {
+        player.play();
+        note.src="img/note.png"
+    } else {
+        player.pause();	
+        note.src="img/note_mute.png"
+    }
+    
+}
